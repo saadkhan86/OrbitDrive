b7127b6de0a2b2528fdb6235b4c22ee371acdab9
Saad Muhammad Bin Ramzan <sk8613013@gmail.com>
2026-09-10 13:48:04 +0500

    feat: add request latency tracking and body validation to server and downgrade tsconfig target to es2015

--- Src/server.ts
+++ Src/server.ts
@@ -1,10 +1,33 @@
 import fastify from "fastify";
 
-
+declare module "fastify"{
+    interface FastifyRequest {
+        startTime : number
+    }
+}
 const Server = fastify({logger:true})
 
+Server.addHook("onRequest",async(request)=>{
+    request.startTime = performance.now()
+})
+Server.addHook("onSend",async(request,reply)=>{
+    reply.header("X-Reponse-Time",`${(request.startTime - performance.now()).toFixed(2)} ms`)
+})
+
 Server.get("/ping",function(request,reply){
-    reply.send({message:"Pong"})
+    return ({message:"Pong"})
+})
+
+Server.post("/",{schema:{
+    body:{
+        type:"object",
+        required:["id"],
+        properties:{
+            id:{type:"string"}
+        }
+    }
+}},async(request,reply)=>{
+    reply.send({type:request.method})
 })
 
 Server.listen({port:8080},(error,port)=>{
