75971e458fcf5cf9fb36aea9c3983b9ccdf3d7ce
Saad Muhammad Bin Ramzan <sk8613013@gmail.com>
2026-09-11 00:21:17 +0500

    feat: implement user authentication using jsonwebtoken and add login service logic

--- Src/Controller/UserController.ts
+++ Src/Controller/UserController.ts
@@ -1,15 +1,21 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { UserService } from "../Services/User.Service";
-import type { SignupValidator } from "../Validators/UserValidator";
+import type {
+  LoginValidator,
+  SignupValidator,
+} from "../Validators/UserValidator";
 
 export const UserController = {
   signup: async (request: FastifyRequest, reply: FastifyReply) => {
-    return reply
-      .status(201)
-      .send(await UserService.signup(request.body as SignupValidator));
+    return reply.status(201).send({
+      message: "User created successfully",
+      user: await UserService.signup(request.body as SignupValidator),
+    });
   },
   login: async (request: FastifyRequest, reply: FastifyReply) => {
-    
-    return reply.status(200).send({ message: "Login route hit" });
+    return reply.status(200).send({
+      message: "User logged in successfully",
+      user: await UserService.login(request.body as LoginValidator),
+    });
   },
 };
--- Src/Services/User.Service.ts
+++ Src/Services/User.Service.ts
@@ -13,7 +13,15 @@ export const UserService = {
       throw new CustomError(409, "User already exists", "USER_ALREADY_EXISTS");
     const passwordHash = await argon2.hash(data.password);
     const user = await UserRepo.create({ ...data, passwordHash });
-    return { message: "Signup Service", user };
+    return user;
   },
-  login: async (data: LoginValidator) => {},
+  login: async (data: LoginValidator) => {
+    const user = await UserRepo.findByEmail(data.email);
+    if (!user)
+      throw new CustomError(401, "User does not exist", "USER_NOT_FOUND");
+    const isMatch = await argon2.verify(user.passwordHash, data.password);
+    if (!isMatch)
+      throw new CustomError(401, "Invalid credentials", "INVALID_CREDENTIALS");
+    return user;
+  },
 };
