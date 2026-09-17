919c873636ea90e52b153c5e5ce74d192f597793
Saad Muhammad Bin Ramzan <sk8613013@gmail.com>
2026-09-17 23:18:36 +0500

    feat:organization_members schema created

---
+++ Src/Database/Schemas/organization_members.ts
@@ -0,0 +1,28 @@
+import {
+  pgTable,
+  uuid,
+  timestamp,
+  integer,
+  PgTimestamp,
+  pgEnum,
+} from "drizzle-orm/pg-core";
+import { organizations } from "./organization.Schema";
+import { users } from "./users.Schema";
+export const pgRoleEnum = pgEnum("organization_member_role", [
+  "OWNER",
+  "ADMIN",
+  "MEMBER",
+  "VIEWER",
+]);
+export const organization_members = pgTable("organization_members", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  organizationId: uuid("organizationId")
+    .references(() => organizations.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  userId: uuid("userId").references(() => users.id),
+  role: pgRoleEnum("role").notNull().default("MEMBER"),
+  createdAt: timestamp("createdAt", { withTimezone: true }).defaultNow(),
+  updatedAt: timestamp("updatedAt", { withTimezone: true }).defaultNow(),
+});
