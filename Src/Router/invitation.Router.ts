64c2ea51d07e07f124e5b6b8174365402a8bd52c
Saad Muhammad Bin Ramzan <sk8613013@gmail.com>
2026-09-21 10:01:46 +0500

    feat (invitation ) :invitation Controller,invitation Service  has been created

--- Src/Router/invitation.Router.ts
+++ Src/Router/invitation.Router.ts
@@ -1,9 +1,9 @@
 import { FastifyInstance } from "fastify";
+import { invitationController } from "../Controller/invitation.Controller";
 
-export const invitationRouter = (app:FastifyInstance)=>{
-    app.post("/",),
-    app.get("/"),
-    app.delete("/:id")
-    app.patch("/:invitationToken")
-
-}
+export const invitationRouter = (app: FastifyInstance) => {
+  app.post("/", invitationController.create);
+  app.get("/", invitationController.get);
+  app.delete("/:id", invitationController.delete);
+  app.patch("/:invitationToken", invitationController.claim);
+};
---
+++ Src/Services/invitation.Service.ts
@@ -0,0 +1,6 @@
+export const invitationService = {
+  create: async (data: any) => {},
+  get: async (data: any) => {},
+  delete: async (data: any) => {},
+  claim: async (data: any) => {},
+};
